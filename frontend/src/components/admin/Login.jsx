fcb961af7f48f3622c4afffa69d2c2f7154a3ba5
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-29 23:04:00 +0200

    feat: Add AdminLogin, SubAdminLogin, and AdminDashboard components

---
+++ frontend/src/components/admin/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminLogin = () => {
+  return (
+    <div>
+      <h1>Admin Login</h1>
+      {/* Add login form here */}
+    </div>
+  );
+};
+
+export default AdminLogin;
---
+++ frontend/src/components/admin/SubAdminLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubAdminLogin = () => {
+  return (
+    <div>
+      <h1>Sub-Admin Login</h1>
+      {/* Add login form here */}
+    </div>
+  );
+};
+
+export default SubAdminLogin;
