834b951b5ba263bba7df87323373626f6d7872bd
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-29 18:59:37 +0200

    chore: Move locales.json to src directory and update import path

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationData from '../public/locales.json'
+import translationData from './locales.json'
 
 const resources = {
   zh: {
