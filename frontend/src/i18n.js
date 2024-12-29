e58bb5e89f50e473cc645c7b96a3db803b7a2215
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-29 19:05:14 +0200

    refactor: Move i18n translations to public folder and update config

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,22 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationData from './locales.json'
-
-const resources = {
-  zh: {
-    translation: translationData.zh
-  },
-  en: {
-    translation: translationData.en
-  }
-};
-
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json'
+    },
     fallbackLng: 'en', // Fallback to English
     interpolation: {
       escapeValue: false,
