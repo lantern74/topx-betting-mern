ed87b8e84c9735ade3413ca12ebf1d34759bd870
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-29 19:12:03 +0200

    refactor: Replace static text with t() hook in HeroBanner.jsx

--- frontend/src/components/pages/HeroBanner.jsx
+++ frontend/src/components/pages/HeroBanner.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const HeroBanner = () => {
+    const { t } = useTranslation();
     const [open, setOpen] = useState(false);
     const OpenModal = () => {
         setOpen(!open)
@@ -12,10 +14,10 @@ const HeroBanner = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-xxl-6 col-md-7">
-                            <h1 className="hero-heading"><span>專業級</span> AI 分析!<br></br>快速、簡單，讓您隨時隨地掌控大局</h1>
-                            <p className="text-lg mb-50 pt-40 pe-xl-5 md-pt-30 md-mb-40">我們的技術不僅提供精確的賽事分析，系統還包含 23,835 支球隊的全面數據，結合最新AI人工智慧分析，推薦最佳投注策略，讓您輕鬆掌控全局。</p>
+                            <h1 className="hero-heading"><span>{t("專業級")}</span> {t("AI 分析!")}<br></br>{t("快速、簡單，讓您隨時隨地掌控大局")}</h1>
+                            <p className="text-lg mb-50 pt-40 pe-xl-5 md-pt-30 md-mb-40">{t("我們的技術不僅提供精確的賽事分析，系統還包含 23,835 支球隊的全面數據，結合最新AI人工智慧分析，推薦最佳投注策略，讓您輕鬆掌控全局。")}</p>
                             <ul className="style-none button-group d-flex align-items-center">
-                                <li className="me-4"><a href="https://t.me/Systemtopxpro" target='_blank' className="demo-btn ripple-btn tran3s">聯絡我們</a></li>
+                                <li className="me-4"><a href="https://t.me/Systemtopxpro" target='_blank' className="demo-btn ripple-btn tran3s">{t("聯絡我們")}</a></li>
                                 {/* <li><a className="fancybox video-icon tran3s" data-fancybox href="#" onClick={OpenModal} ><i className="fas fa-play" /></a></li> */}
                             </ul>
                         </div>
