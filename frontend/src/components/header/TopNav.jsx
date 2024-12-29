30cdb2ec7f3be06a2a8b126e9cdb5ff960df1199
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-29 20:33:10 +0200

    feat: Add i18n support for login button in TopNav

--- frontend/src/components/header/TopNav.jsx
+++ frontend/src/components/header/TopNav.jsx
@@ -1,9 +1,11 @@
 import React, {Fragment, useState} from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import MobileMenu from './MobileMenu';
 import ThemeMainMenu from './ThemeMainMenu';
 
 const TopNav = () => {
+    const { t } = useTranslation();
 
   const [navbar, setNavbar] = useState(false);
   
@@ -36,7 +38,7 @@ const TopNav = () => {
                             <Link to="/" className="d-block"><img className='responsive-logo' src="images/logo/topx-logo.png" alt=""/></Link>
                         </div>
                         <div className="right-widget d-flex align-items-center ms-auto order-lg-3">
-                            <Link to="/login" className="send-msg-btn tran3s d-none d-lg-block">登入</Link>
+                            <Link to="/login" className="send-msg-btn tran3s d-none d-lg-block">{t("登入")}</Link>
                         </div>
                         <nav className="navbar navbar-expand-lg order-lg-2">
                             <div className="collapse navbar-collapse" id="navbarNav">
