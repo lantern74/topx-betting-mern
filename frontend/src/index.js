29197170ed99dec590f38eeb9216ee69a1642827
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 08:34:29 +0200

    feat: Add tailwind css and import index.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import './assets/main.scss';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
