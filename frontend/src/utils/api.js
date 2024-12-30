ae2865e0afec23f7ea26421b7fbb7551a52db084
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 14:09:01 +0200

    feat: Add request interceptor to include auth token in API requests

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import useAuthStore from '../store/authStore';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000',
@@ -15,6 +16,21 @@ const setAuthHeader = (token) => {
     delete api.defaults.headers.common['Authorization'];
   }
 };
+
+// Add a request interceptor to include the token
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 
 // Function to handle API errors
 const handleApiError = (error) => {
