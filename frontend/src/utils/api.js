a1a68dd6d7b220717c9e049dd79b5de52f8c0b7c
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 22:01:07 +0200

    fix: Use /api as baseURL in api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const api = axios.create({
-  baseURL: '/',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
