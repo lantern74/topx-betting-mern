d73e94625a52a3a90ceae12f18cf650beb0d0483
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 08:14:50 +0200

    nice

--- backend/routes/match.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMatchData, getMatchResult } = require('../controllers/matchController');
-
-router.get('/match-data', getMatchData);
-router.get('/match-result/:id', getMatchResult);
-
-module.exports = router;
--- frontend/src/components/form/Login.jsx
+++ frontend/src/components/form/Login.jsx
@@ -1,34 +1,36 @@
-import React, {Fragment} from 'react';
-import {Helmet} from 'react-helmet';
-import { useTranslation } from 'react-i18next';
-import LoginForm from './LoginForm';
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
-    const { t } = useTranslation();
-    return (
-        <Fragment>
-            <div className="main-page-wrapper">
-                <Helmet>
-                    <title>TOP X</title>
-                </Helmet>
-                <div className="contact-section-one ">
-                    <div className="mt-200 lg-mt-70">
-                        <div className="container">
-                            <div className="row gx-xxl-5">
-                                <div className="col-lg-6 d-flex order-lg-last">
-                                    <div className="form-style-one">
-                                        <h3 className="form-title pb-40 lg-pb-20">{t("會員登入")}</h3>
-                                        <LoginForm/>
-                                    </div>
-                                    {/* /.form-style-one */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  const { t } = useTranslation();
+  return (
+    <Fragment>
+      <div className="main-page-wrapper">
+        <Helmet>
+          <title>TOP X</title>
+        </Helmet>
+        <div className="contact-section-one ">
+          <div className="mt-200 lg-mt-70">
+            <div className="container">
+              <div className="row gx-xxl-5">
+                <div className="col-lg-6 d-flex order-lg-last">
+                  <div className="form-style-one">
+                    <h3 className="form-title pb-40 lg-pb-20">
+                      {t("會員登入")}
+                    </h3>
+                    <LoginForm />
+                  </div>
+                  {/* /.form-style-one */}
                 </div>
+              </div>
             </div>
-        </Fragment>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-export default Login
+export default Login;
