505c455e24b6d16f2251bf71fc3f71df4ac9bc0d
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 09:31:49 +0200

    feat: Add language switcher and style logo text

--- frontend/src/components/header/TopNav.jsx
+++ frontend/src/components/header/TopNav.jsx
@@ -4,30 +4,37 @@ import { useTranslation } from 'react-i18next';
 import MobileMenu from './MobileMenu';
 import ThemeMainMenu from './ThemeMainMenu';
 import useAuthStore from '../../store/authStore';
+import i18n from '../../i18n';
 
 const TopNav = () => {
     const { t } = useTranslation();
     const { isAuthenticated } = useAuthStore();
-
-  const [navbar, setNavbar] = useState(false);
-  
-  const [modalIsOpen, setIsOpen] = useState(false);
+    const [navbar, setNavbar] = useState(false);
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [languageDropdownOpen, setLanguageDropdownOpen] = useState(false);
 
     const closeModal = () => {
         setIsOpen(!modalIsOpen);
     }
-    
 
-  const toggleMenu =()=>{
-    if(window.scrollY >= 68) {
-      setNavbar(true)
-    } else{
-      setNavbar(false)
+    const toggleMenu =()=>{
+        if(window.scrollY >= 68) {
+            setNavbar(true)
+        } else{
+            setNavbar(false)
+        }
     }
-  }
 
-  window.addEventListener('scroll', toggleMenu);
+    window.addEventListener('scroll', toggleMenu);
 
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        setLanguageDropdownOpen(false);
+    };
+
+    const toggleLanguageDropdown = () => {
+        setLanguageDropdownOpen(!languageDropdownOpen);
+    };
 
 
     return (
@@ -37,16 +44,34 @@ const TopNav = () => {
                 <div className="inner-content">
                     <div className="d-flex align-items-center">
                         <div className="logo order-lg-0">
-                            <Link to="/" className="d-block"><img className='responsive-logo' src="images/logo/topx-logo.png" alt=""/></Link>
+                            <Link to="/" className="d-block">
+                                <span className="logo-text">TOPX</span>
+                                {/* <img className='responsive-logo' src="images/logo/topx-logo.png" alt=""/> */}
+                            </Link>
                         </div>
                         <div className="right-widget d-flex align-items-center ms-auto order-lg-3">
+                            <div className="language-switcher" onMouseEnter={toggleLanguageDropdown} onMouseLeave={toggleLanguageDropdown}>
+                                <div className="current-language">
+                                    {i18n.language === 'en' ? <img src="/images/icon/us.svg" alt="English" /> : <img src="/images/icon/china.svg" alt="Chinese" />}
+                                </div>
+                                {languageDropdownOpen && (
+                                    <div className="language-dropdown">
+                                        <button onClick={() => changeLanguage('en')}>
+                                            <img src="/images/icon/us.svg" alt="English" /> English
+                                        </button>
+                                        <button onClick={() => changeLanguage('zh')}>
+                                            <img src="/images/icon/china.svg" alt="Chinese" /> 中文
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
                             {!isAuthenticated && (
                                 <Link to="/login" className="send-msg-btn tran3s d-none d-lg-block">{t("登入")}</Link>
                             )}
                         </div>
                         <nav className="navbar navbar-expand-lg order-lg-2">
                             <div className="collapse navbar-collapse" id="navbarNav">
-                                <ThemeMainMenu/> 
+                                <ThemeMainMenu/>
                             </div>
                         </nav>
                     </div>
