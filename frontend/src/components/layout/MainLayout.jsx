1973e100163c54401fcd5e9097795ab518450def
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 07:13:15 +0200

    refactor: Remove Footer from MainLayout

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react';
 import TopNav from '../header/TopNav';
-import Footer from '../footer/Footer';
 
 const MainLayout = ({ children }) => {
   return (
     <Fragment>
       <TopNav />
       {children}
-      <Footer />
     </Fragment>
   );
 };
