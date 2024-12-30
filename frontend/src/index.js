d98cc46b1d4af21de0cedcb5767e1e4750a53021
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 13:36:00 +0200

    bad

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import './assets/main.scss';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import './global.scss';
+// import './global.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
