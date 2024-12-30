cec0a055d4a75c5d93bd4701f4e05eae76d13d94
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-30 09:16:49 +0200

    fix: Wrap App with QueryClientProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,17 @@ import './assets/main.scss';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import './global.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>
+  <QueryClientProvider client={queryClient}>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
