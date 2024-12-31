dea221c0ce141ea6d1d19542d9cb45f984a444be
qayyim <aboalqayyimash.shami@gmail.com>
2024-12-31 22:59:14 +0200

    feat: Handle 401 errors by logging out and redirecting

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import useAuthStore from '../store/authStore';
+import { useNavigate } from 'react-router-dom';
 
 const api = axios.create({
   baseURL: '/api',
@@ -7,7 +9,6 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
 
 // Add a request interceptor to include the session cookie
 api.interceptors.request.use(
@@ -26,18 +27,31 @@ api.interceptors.request.use(
 
 // Function to handle API errors
 const handleApiError = (error) => {
-  console.error('API Error:', error);
-  if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    return error.response.data.message || 'An error occurred';
-  } else if (error.request) {
-    // The request was made but no response was received
-    return 'No response from server';
-  } else {
-    // Something happened in setting up the request that triggered an Error
-    return error.message || 'An error occurred';
-  }
+    console.error('API Error:', error);
+    const navigate = useNavigate();
+    const logout = useAuthStore.getState().logout;
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        if (error.response.status === 401) {
+            // Handle unauthorized error (e.g., session expired)
+            logout();
+            const userRole = useAuthStore.getState().userRole;
+            if (userRole === 'main' || userRole === 'sub') {
+                navigate('/admin/login');
+            } else {
+                navigate('/login');
+            }
+            return 'Session expired, please log in again.';
+        }
+        return error.response.data.message || 'An error occurred';
+    } else if (error.request) {
+        // The request was made but no response was received
+        return 'No response from server';
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        return error.message || 'An error occurred';
+    }
 };
 
 export { api, handleApiError };
