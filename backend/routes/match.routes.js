9dc105c1bc6a1f5b5dfcf28ee8fb0b7ea5f3a9f3
qayyim <aboalqayyimash.shami@gmail.com>
2025-01-03 17:25:28 +0200

    refactor: Make match routes accessible to any logged-in user

--- backend/routes/match.routes.js
+++ backend/routes/match.routes.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const MatchController = require('../controllers/match.controller');
-const { authenticateAdmin } = require('../middleware/authMiddleware');
 
 /**
  * @route GET /match-data
  * @desc Get all match data.
  * @access Public
  */
-router.get('/match-data', authenticateAdmin, MatchController.getMatchData);
+router.get('/match-data', MatchController.getMatchData);
 
 /**
  * @route GET /match-result/:id
  * @desc Get match result by ID.
  * @access Public
  */
-router.get('/match-result/:id', authenticateAdmin, MatchController.getMatchResult);
+router.get('/match-result/:id', MatchController.getMatchResult);
 
 module.exports = router;
